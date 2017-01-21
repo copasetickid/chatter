d0d9d7e345e7d1589734ca2d88e3729043302b20
Rushaine McBean <rushaine.mcbean@gmail.com>
2017-01-21 12:10:13 -0500

    Allows users to respond to conversations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   root 'conversations#index'
-  
+
   devise_for :users
+
+  resources :personal_messages, only: [:create]
+  resources :conversations, only: [:index, :show]
 end
